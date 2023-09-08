0f1acba7d60b5f8ef3bf722e7dc5e14ed3ac9334
Juan Rodriguez <juansebastianrodriguezmateus7b@gmail.com>
2023-09-08 18:14:17 -0500

    Advance Project

--- ArquimedesPrinciple/src/co/edu/uptc/model/ManagerModel.java
+++ ArquimedesPrinciple/src/co/edu/uptc/model/ManagerModel.java
@@ -1,15 +1,35 @@
 package co.edu.uptc.model;
 
 import co.edu.uptc.presenter.ContractArquimedesPrinciple;
+import co.edu.uptc.utils.Utils;
 
 public class ManagerModel implements ContractArquimedesPrinciple.model {
 
     ContractArquimedesPrinciple.presenter presenter;
+    private boolean start;
+
+    public ManagerModel(){
+        start = true;
+        refresh();
+    }
 
     @Override
     public void setPresenter(ContractArquimedesPrinciple.presenter presenter) {
         this.presenter = presenter;
     }
 
+    @Override
+    public void refresh() {
 
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(start){
+                    presenter.refresh();
+                    Utils.sleepP(100);
+                }
+            }
+        });
+        thread.start();
+    }
 }
--- ArquimedesPrinciple/src/co/edu/uptc/view/DashBoard.java
+++ ArquimedesPrinciple/src/co/edu/uptc/view/DashBoard.java
@@ -3,10 +3,29 @@ package co.edu.uptc.view;
 import co.edu.uptc.presenter.ContractArquimedesPrinciple;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class DashBoard extends JFrame implements ContractArquimedesPrinciple.view {
 
     ContractArquimedesPrinciple.presenter presenter;
+    private PanelValueObject panelObject;
+
+    public DashBoard(){
+        config();
+        initComponent();
+    }
+
+    public void config(){
+        this.setSize(new Dimension(800,600));
+        this.setLayout(null);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public void initComponent(){
+        createPanelObject();
+    }
 
     @Override
     public void setPresenter(ContractArquimedesPrinciple.presenter presenter) {
@@ -17,4 +36,19 @@ public class DashBoard extends JFrame implements ContractArquimedesPrinciple.vie
     public void start() {
         this.setVisible(true);
     }
+
+    @Override
+    public void refresh() {
+        this.repaint();
+        panelObject.changeMassExact();
+        panelObject.changeVolumeExact();
+    }
+
+    private void createPanelObject(){
+        panelObject = new PanelValueObject();
+        panelObject.setBounds(0,0,800,80);
+        add(panelObject);
+    }
+
+
 }
